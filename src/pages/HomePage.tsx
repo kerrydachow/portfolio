fec46d2025d564a08a8e3c1820f4e69c656a0fec
kerrydachow <kerry_chow@hotmail.ca>
2023-09-06 16:16:00 -0700

    Add homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/header/Navbar";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default HomePage;
