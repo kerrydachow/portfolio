d24b83ae8c5e0c941da40072853899aa076d468c
kerrydachow <kerry_chow@hotmail.ca>
2023-09-06 16:08:44 -0700

    Add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,25 @@
+const navLinks: Array<{ id: string; title: string }> = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+];
+
+const infoLinks: { github: string; linkedin: string } = {
+  github: "https://github.com/kerrydachow",
+  linkedin: "https://www.linkedin.com/in/kerrykchow/",
+};
+
+export { navLinks, infoLinks };
