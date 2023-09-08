02edde3043fc978c73bd3b62a5cf7c8cbea15f01
kerrydachow <kerry_chow@hotmail.ca>
2023-09-07 18:47:23 -0700

    Add asset images

---
+++ src/assets/index.ts
@@ -0,0 +1,29 @@
+import aws from "./technologies/aws.png";
+import c from "./technologies/c.png";
+import docker from "./technologies/docker.png";
+import java from "./technologies/java.png";
+import javascript from "./technologies/javascript.png";
+import mongodb from "./technologies/mongodb.png";
+import nodejs from "./technologies/nodejs.png";
+import python from "./technologies/python.png";
+import react from "./technologies/react.png";
+import sql from "./technologies/sql.png";
+import typescript from "./technologies/typescript.png";
+import mlc from "./mlc.png"
+import paymentsource from "./payment_source.jpeg"
+
+export default {
+  aws,
+  c,
+  docker,
+  java,
+  javascript,
+  mongodb,
+  nodejs,
+  python,
+  react,
+  sql,
+  typescript,
+  mlc,
+  paymentsource
+};
