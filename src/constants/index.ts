03678f6b0f8d610ad69198c1c7c7b97788d04788
kerrydachow <kerry_chow@hotmail.ca>
2023-09-07 18:14:06 -0700

    Add constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,4 +22,111 @@ const infoLinks: { github: string; linkedin: string } = {
   linkedin: "https://www.linkedin.com/in/kerrykchow/",
 };
 
-export { navLinks, infoLinks };
+const skills: Array<{ skill: string; icon: string }> = [
+  {
+    skill: "React JS",
+    icon: "reactjs",
+  },
+  {
+    skill: "Python",
+    icon: "python",
+  },
+  {
+    skill: "Java",
+    icon: "java",
+  },
+  {
+    skill: "TypeScript",
+    icon: "typescript",
+  },
+  {
+    skill: "JavaScript",
+    icon: "javascript",
+  },
+  {
+    skill: "Docker",
+    icon: "docker",
+  },
+  {
+    skill: "SQL",
+    icon: "sql",
+  },
+  {
+    skill: "Node.js",
+    icon: "nodejs",
+  },
+  {
+    skill: "Express.js",
+    icon: "expressjs",
+  },
+  {
+    skill: "MongoDB",
+    icon: "mongodb",
+  },
+  {
+    skill: "AWS",
+    icon: "aws",
+  },
+];
+
+const expertise: Array<{
+  title: string;
+  description: string;
+  icons: Array<string>;
+}> = [
+  {
+    title: "Software Development",
+    description: "Experience in both functional programming and OOP.",
+    icons: ["Java.png", "Python.png", "C.png", "SQL.png"],
+  },
+  {
+    title: "Cloud/DevOps Engineer",
+    description: "Experience with AWS, building CI/CD pipelines, and Docker.",
+    icons: ["AWS.png", "Docker.png"],
+  },
+  {
+    title: "Full Stack Engineer",
+    description:
+      "Experience in developing web applications from start to finish.",
+    icons: ["JavaScript.png", "TypeScript.png", "ReactJS.png", "MongoDB.png"],
+  },
+];
+
+const experiences: Array<{
+  title: string;
+  company_name: string;
+  icon: string;
+  iconBg: string;
+  date: string;
+  points: Array<string>;
+}> = [
+  {
+    title: "Software Developer",
+    company_name: "PaymentSource",
+    icon: "payment_source.jpeg",
+    iconBg: "#383E56",
+    date: "Apr 2022 - May 2022",
+    points: [
+      "Engineered a web application that seamlessly transforms existing JSON data within the database into an editable web form and subsequently persisted the modified data back to the database",
+      "Attained 100% consistency in data modification without unintended side-effects",
+      "Conducted unit tests on code, and documented the process to enhance integration with legacy software",
+      "Technologies: ASP.NET, Visual Basic, JavaScript, HTML, CSS, GitHub, AWS DynamoDB, jQuery, Bootstrap 4"
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "MyLivingCity",
+    icon: "mlc2.jpeg",
+    iconBg: "#E6DEDD",
+    date: "Sep 2022 - Dec 2022",
+    points: [
+      "Implemented moderation features for user and post management",
+      "Reduced API costs by 94.4% by identifying the cause and implementing caching",
+      "Reduced moderation dashboard page load time by 3 seconds through optimizing data filtration",
+      "Reviewed, documented, and automated testing for newly implemented features",
+      "TypeScript, PostgreSQL, ReactJS, Node.js, Prisma, Bootstrap, GitHub, AWS Amplify",
+    ],
+  }
+];
+
+export { navLinks, infoLinks, skills, expertise, experiences };
