3e86210c8679293c7e0419888fec35c195940a7b
kerrydachow <kerry_chow@hotmail.ca>
2023-09-10 23:12:28 -0700

    Add summary for work experience

--- src/pages/HomePage/experience/WorkExperience.tsx
+++ src/pages/HomePage/experience/WorkExperience.tsx
@@ -2,7 +2,7 @@ import { SectionWrapper } from "@/hoc";
 import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { motion } from "framer-motion";
-import { textVariant } from "../../../utils/motion";
+import { fadeIn, textVariant } from "../../../utils/motion";
 import { experiences } from "../../../constants";
 import ExperienceCard from "./ExperienceCard";
 
@@ -17,6 +17,22 @@ const WorkExperience = () => {
           Work Experience
         </h2>
       </motion.div>
+      <div className="w-full flex justify-center">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-[17px] text-center max-w-3xl leading-[30px]"
+        >
+          Thanks to BCIT's Industry Sponsored Student Projects program, I've had
+          the privilege to complete two project practicums. The opportunity to
+          work on genuine IT and software development projects directly from
+          industry sponsors was invaluable. Through this program, I was not only
+          able to apply my skills in real-life scenarios but also understand the
+          comprehensive development life-cycle. Working in teams, sometimes
+          individually, under the guidance of our industry sponsors has
+          ingrained in me the confidence, skills, and experience that I can now
+          carry forward into the professional world.
+        </motion.p>
+      </div>
       <div className="mt-5 flex flex-col">
         <VerticalTimeline lineColor="rgb(148 163 184)" className="">
           {experiences.map((experience, index) => (
