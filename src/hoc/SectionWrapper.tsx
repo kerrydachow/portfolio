732acc58546676f4f9227392e5b470e2a24fc4ba
kerrydachow <kerry_chow@hotmail.ca>
2023-09-11 19:42:03 -0700

    Update css styles

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -7,12 +7,19 @@ const StarWrapper = (Component: React.FC, idName: string) =>
     return (
       <motion.section
         variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
-        className={`sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0`}
+        className={`sm:px-16 px-6 sm:py-16 py-16 max-w-7xl mx-auto relative z-0`}
       >
-        <span className='hash-span' id={idName}>
+        <span
+          style={{
+            marginTop: "-100px",
+            paddingBottom: "100px",
+            display: "block",
+          }}
+          id={idName}
+        >
           &nbsp;
         </span>
 
