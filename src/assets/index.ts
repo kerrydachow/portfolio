ed72d803f2740637be75d264739451f356e8f230
kerrydachow <kerry_chow@hotmail.ca>
2023-09-12 16:15:42 -0700

    Add asset images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -16,16 +16,27 @@ import mlc from "./mlc.png";
 import paymentsource from "./payment_source.jpeg";
 
 // Project Pictures
-import sss1 from "./projects/sss-1.png"
-import sss2 from "./projects/sss-2.png"
-import sss3 from "./projects/sss-3.png"
-import sss4 from "./projects/sss-4.png"
+import sss1 from "./projects/sss-1.png";
+import sss2 from "./projects/sss-2.png";
+import sss3 from "./projects/sss-3.png";
+import sss4 from "./projects/sss-4.png";
+
+import socialio1 from "./projects/socialio-1.png";
+import socialio2 from "./projects/socialio-2.png";
+import socialio3 from "./projects/socialio-3.png";
+import socialio4 from "./projects/socialio-4.png";
+
+import ptb1 from "./projects/python-tba-game-1.png";
+import ptb2 from "./projects/python-tba-game-2.png";
+import ptb3 from "./projects/python-tba-game-3.png";
+import ptb4 from "./projects/python-tba-game-4.png";
+import ptb5 from "./projects/python-tba-game-5.png";
+import ptb6 from "./projects/python-tba-game-6.png";
+import ptb7 from "./projects/python-tba-game-7.png";
 
 // Me
-import me1 from "./me-1.jpg"
-import me2 from "./me-2.png"
-
-import socialio1 from "./projects/socialio-1.png"
+import me1 from "./me-1.jpg";
+import me2 from "./me-2.png";
 
 export default {
   aws,
@@ -47,6 +58,16 @@ export default {
   sss3,
   sss4,
   socialio1,
+  socialio2,
+  socialio3,
+  socialio4,
+  ptb1,
+  ptb2,
+  ptb3,
+  ptb4,
+  ptb5,
+  ptb6,
+  ptb7,
   me1,
   me2,
 };
