277ac8b61c7766658ccaf996b79a2dc006f55a9e
kerrydachow <kerry_chow@hotmail.ca>
2023-09-12 18:24:35 -0700

    Modify HomePage to include components

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,21 @@
-import AboutSection from "@/pages/HomePage/about/AboutSection";
-import WorkExperience from "@/pages/HomePage/experience/WorkExperience";
 import Navbar from "@/components/header/Navbar";
-import React from "react";
+import AboutSection from "./about/AboutSection";
+import WorkExperience from "./experience/WorkExperience";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
+import HeroSection from "./hero/HeroSection";
+import Footer from "@/components/footer/Footer";
 
 const HomePage = () => {
   return (
-    <div className="z-50 container">
+    <div className="z-50">
       <Navbar />
+      <HeroSection />
       <AboutSection />
       <WorkExperience />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 };
